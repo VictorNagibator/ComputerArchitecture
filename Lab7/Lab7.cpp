3ec2f30c1ca630122bf7d7ede20f76c096408fef
Victor <vityadonskoy2004@mail.ru>
2024-02-10 15:47:59 +0700

    Add project files.

---
+++ Lab7/Lab7.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "stdio.h"
+
+int main()
+{
+    int a, b, c, d, y;
+
+    printf("Input A, B, C, D: ");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    __asm 
+    {
+        
+    }
+    printf("\nResult: %d", y);
+
+    return 0;
+}
