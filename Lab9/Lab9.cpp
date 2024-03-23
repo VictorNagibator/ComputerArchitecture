5a0512d9762e95ad2e5a5588ae69cb436fada9d3
Victor <vityadonskoy2004@mail.ru>
2024-03-23 17:29:36 +0700

    Optimized OpenMP algorithm

--- Lab9/Lab9.cpp
+++ Lab9/Lab9.cpp
@@ -1,88 +1,85 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <windows.h>
-#include <omp_llvm.h>
+#include <omp.h>
 
 int main()
 {
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
 
-	const int n = 10000, m = 10;
-	int arr[n][m];
-	double startTime;
-	int maxRow = 0;
+    const int n = 100, m = 10;
+    int arr[n][m];
+    int maxRow = -1;
+    double startTime;
+    int a;
 
-	//заполнение массива
-	srand(time(NULL));
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			arr[i][j] = (-16384) + rand() % (32768);
-		}
-	}
+    //заполнение массива
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            arr[i][j] = (-16384) + rand() % (32768);
+        }
+    }
 
-	printf("Максимальное количество потоков: %d\n\n", omp_get_max_threads());
-	printf("Исходный массив %d на %d:\n", n, m);
-	//вывод массива
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			printf("%6d ", arr[i][j]);
-		}
-		printf("\n");
-	}
+    //вывод массива
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%6d ", arr[i][j]);
+        }
+        printf("\n");
+    }
 
-	int a;
-	printf("Введите число a: ");
-	do
-	{
-		scanf("%d", &a);
-		if (a == 0) printf("Число a должно быть не равно 0. Повторите ввод: ");
-	} while (a == 0);
+    printf("\nВведите число a: ");
+    do {
+        scanf("%d", &a);
+        if (a == 0) printf("Число a должно быть не равно 0. Повторите ввод: ");
+    } while (a == 0);
 
-	//вариант алгоритма в обычном режиме
-	startTime = omp_get_wtime();
-	for (int i = 0; i < n; i++) {
-		bool areAllElementsMultipleOfA = true;
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] % a != 0) {
-				areAllElementsMultipleOfA = false;
-				break;
-			}
-		}
-		if (areAllElementsMultipleOfA) {
-			maxRow = i;
-		}
-	}
+    printf("\nРазмеры массива: %d на %d:\n\n", n, m);
 
-	printf("\nМаксимальный номер строки с элементами, кратными числу a: %d\n", maxRow);
-	printf("Стандартное время алгоритма (без OpenMP) в мс = %f\n\n", omp_get_wtime() - startTime);
+    //вариант алгоритма в обычном режиме
+    startTime = omp_get_wtime();
+    for (int i = 0; i < n; i++) {
+        bool areAllElementsMultipleOfA = true;
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] % a != 0) {
+                areAllElementsMultipleOfA = false;
+                break;
+            }
+        }
+        if (areAllElementsMultipleOfA) {
+            maxRow = i;
+        }
+    }
 
+    if (maxRow != -1) printf("Максимальный номер строки с элементами, кратными числу a: %d\n", maxRow);
+	else printf("В массиве нет строк, в которых все элементы кратны числу a\n");
+    printf("Стандартное время алгоритма (без OpenMP) в мс = %f\n\n", omp_get_wtime() - startTime);
 
-	//вариант алгоритма в многопоточном режиме
-	omp_set_num_threads(12);
-	maxRow = 0;
-	startTime = omp_get_wtime();
-	#pragma omp parallel for shared(maxRow)
-	for (int i = 0; i < n; i++) {
-		bool areAllElementsMultipleOfA = true;
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] % a != 0) {
-				areAllElementsMultipleOfA = false;
-				break;
-			}
-		}
-		if (areAllElementsMultipleOfA) {
-			#pragma omp critical
-			{
-				if (i > maxRow) {
-					maxRow = i;
-				}
-			}
-		}
-	}
+    //вариант алгоритма в многопоточном режиме
+    maxRow = -1;
+    startTime = omp_get_wtime();
+    #pragma omp parallel for reduction(max:maxRow)
+    for (int i = 0; i < n; i++) {
+        int localMaxRow = 0; //локальная переменная для каждого потока
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] % a != 0) {
+                localMaxRow = -1; //если не все элементы кратны a, не учитываем строку
+                break;
+            }
+        }
 
-	printf("Максимальный номер строки с элементами, кратными числу a: %d\n", maxRow);
-	printf("Время алгоритма с использованием директив распараллеливания в мс = %f\n", omp_get_wtime() - startTime);
-	
-	return 0;
+        if (localMaxRow != -1) {
+            localMaxRow = i; //если все элементы кратны a, запоминаем строку
+        }
+
+        if (localMaxRow > maxRow) maxRow = localMaxRow; // сравниваем и обновляем максимальное значение строки
+    }
+
+    if (maxRow != -1) printf("Максимальный номер строки с элементами, кратными числу a: %d\n", maxRow);
+    else printf("В массиве нет строк, в которых все элементы кратны числу a\n");
+    printf("Время алгоритма с использованием директив распараллеливания (OpenMP) в мс = %f\n", omp_get_wtime() - startTime);
+
+    return 0;
 }
